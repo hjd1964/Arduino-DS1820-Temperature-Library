7e8ffcbef7aebb841dcf26617d155e50ec495bbd
Howard Dutton <hjd1964@gmail.com>
2023-03-25 15:21:44 -0400

    Update

--- DallasTemperature.cpp
+++ DallasTemperature.cpp
@@ -3,7 +3,7 @@
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
 
-#include "DallasTemp.h"
+#include "DallasTemperature.h"
 
 #if ARDUINO >= 100
 #include "Arduino.h"
